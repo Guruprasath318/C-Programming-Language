8af5dab7b490e62508a1bc150324c91e42246057
Guruprasath318 <guruprasathkkm@gmail.com>
2025-08-04 22:14:53 +0530

    C program

---
+++ for.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    printf("%f\n",pow(2, 3));
+    printf("%f\n",ceil(2.3));
+    printf("%f\n",floor(2.7));
+    printf("%f\n",round(2.5));
+    printf("%f\n",round(2.4));
+    return 0;
+}
